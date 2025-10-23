150ea9fdfd9cecb60c4a590ea80acddb923d5422
Molloy <2753219155@qq.com>
2025-10-23 13:53:33 +0800

    功能添加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -43,8 +43,7 @@ export default defineNuxtConfig({
     },
     runtimeConfig: {
         public: {
-            apiBase:
-                process.env.NUXT_PUBLIC_API_URL || 'http://127.0.0.1:8000/api/',
+            apiBase: process.env.NUXT_PUBLIC_API_URL || 'https://kotae.cn/api/',
         },
     },
     imports: {
